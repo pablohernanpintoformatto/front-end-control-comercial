ccd78a669146dfd87b6c630f3ae33519ae0373ef
pablohernanpintoformatto <pablo.pinto@formatto.cl>
2025-09-24 17:45:32 -0300

    cierre de dia 24-09-25

--- src/app/pages/front-page/front-page.ts
+++ src/app/pages/front-page/front-page.ts
@@ -2,17 +2,21 @@
 
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatMenuModule } from '@angular/material/menu';
 @Component({
   selector: 'app-front-page',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatButtonModule, MatToolbarModule, MatSidenavModule, MatMenuModule], // Agregar los módulos aquí
   templateUrl: './front-page.html',
-  styleUrl: './front-page.css'
+  styleUrls: ['./front-page.css']
 })
 export class FrontPage {
-  constructor(private router: Router) {}
-
+  constructor(private router: Router) { }
+  showFiller = false;
   logout() {
     localStorage.removeItem('token'); // Elimina el token
     this.router.navigate(['/login']); // Redirige al login
